6c6a52e43ef50096724da19d8ea4da15f2a0613d
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-01 18:07:30 +0530

    Spring Boot

--- SmartContactManager/src/main/resources/static/js/script.js
+++ SmartContactManager/src/main/resources/static/js/script.js
@@ -1,6 +1,20 @@
 /*alert("hello pranav");*/
 console.log("I am Pranav Mahajan");
 
+// function toggleSidebar(){
 
+// }
+
+const toggleSidebar=()=>{
+    if($(".sidebar").is(":visible")){
+        //true mean band karana
+        $(".sidebar").css("display","none");
+        $(".content").css("margin-left","0%");
+    }else{
+        //false mean show karana
+        $(".sidebar").css("display","block");
+        $(".content").css("margin-left","20%");
+    }
+};
 
 
