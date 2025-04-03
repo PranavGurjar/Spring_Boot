95ceb1eda9f71fcd0788c96dbfed7646f3e23b19
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-03 13:46:56 +0530

    Spring Boot

--- SmartContactManager/src/main/java/com/scm/controller/UserController.java
+++ SmartContactManager/src/main/java/com/scm/controller/UserController.java
@@ -5,9 +5,11 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.scm.dao.UserRepository;
+import com.scm.entities.Contact;
 import com.scm.entities.User;
 
 @Controller
@@ -17,8 +19,8 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@RequestMapping("/index")
-	public String dashboard(Model model, Principal principal){
+	@ModelAttribute
+	public void addCommonData(Model model, Principal principal) {
 		String userName = principal.getName();
 		System.out.println("UserName : "+userName);
 		
@@ -26,8 +28,24 @@ public class UserController {
 		User user = userRepository.getUserByUserName(userName);
 		System.out.println("USER : "+user);
 		model.addAttribute("user", user);
-		
+	}
+	
+	
+	@RequestMapping("/index")
+	public String dashboard(Model model, Principal principal){
+		model.addAttribute("title", "User Dashboard");
 		return "user/user_dashboard";
 	}
+	
+	
+	
+	//add contact handler
+	@RequestMapping("/addContact")
+	public String addContactForm(Model model) {
+		model.addAttribute("title", "Add Contact");
+		model.addAttribute("contact", new Contact());
+		return "user/addContact";
+	}
+	
 	
 }
