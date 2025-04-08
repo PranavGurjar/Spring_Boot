7a55aa1d153fbcfc1c80e6fc1d78966f786abbdc
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-08 14:52:23 +0530

    Spring Boot

---
+++ Email_API/src/main/java/com/email/controller/EmailController.java
@@ -0,0 +1,13 @@
+package com.email.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @RequestMapping("/welcome")
+    public String welcome(){
+        return "Hello sir please welcome";
+    }
+}
