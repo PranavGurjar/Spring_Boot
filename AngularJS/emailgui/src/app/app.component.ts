9b1e251c258c7351ccbe2c2db0ba702de0995d34
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-09 13:35:56 +0530

    Spring Boot

--- AngularJS/emailgui/src/app/app.component.ts
+++ AngularJS/emailgui/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar.component'; 
+import { HomeComponent } from './component/home/home.component'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'emailgui';
