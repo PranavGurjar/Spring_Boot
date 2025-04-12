101b704f4968a70f0a473cb19d1924ccc00021c5
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-12 13:53:29 +0530

    Spring Boot

---
+++ JWT_Authentication_Server/JWT_Authentication_Server/src/main/java/com/jwt/JwtAuthenticationServerApplication.java
@@ -0,0 +1,13 @@
+package com.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationServerApplication.class, args);
+	}
+
+}
