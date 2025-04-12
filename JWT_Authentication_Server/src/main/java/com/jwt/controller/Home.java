d9e3209fca6cbf5dadb7741930fd5095d4545566
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-12 14:55:58 +0530

    Spring Boot

---
+++ JWT_Authentication_Server/src/main/java/com/jwt/controller/Home.java
@@ -0,0 +1,18 @@
+package com.jwt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+
+
+
+    @RequestMapping("/")
+    public String welcome(){
+        String text = "this is private page";
+        text+="this page is not allowed to unauthenicated users";
+        return text;
+    }
+}
