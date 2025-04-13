c5cb9d8e8a361347a3dd3af2f4f1d157f1eeda4c
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-13 12:57:48 +0530

    Spring Boot

--- JWT_Authentication_Server/src/main/java/com/jwt/controller/Home.java
+++ JWT_Authentication_Server/src/main/java/com/jwt/controller/Home.java
@@ -1,6 +1,7 @@
 package com.jwt.controller;
 
 
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
