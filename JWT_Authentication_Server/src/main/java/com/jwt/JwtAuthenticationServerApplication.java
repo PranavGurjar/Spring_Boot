e2709dede08f9319ef76d778759b9c42063c6b66
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-13 15:45:30 +0530

    Spring Boot

--- JWT_Authentication_Server/src/main/java/com/jwt/JwtAuthenticationServerApplication.java
+++ JWT_Authentication_Server/src/main/java/com/jwt/JwtAuthenticationServerApplication.java
@@ -1,13 +1,42 @@
 package com.jwt;
 
+import com.jwt.model.User;
+import com.jwt.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
+
 @SpringBootApplication
-public class JwtAuthenticationServerApplication {
+public class JwtAuthenticationServerApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	Random random = new Random();
+
+	public void createUser(){
+		User user = new User();
+		Long id = new Long(random.nextInt(100));
+		user.setId(id);
+		user.setEmail("user"+id+"@gmail.com");
+		user.setUsername("user"+id);
+		user.setPassword("user"+id);
+		user.setEnabled(true);
+		user.setRole("Admin");
+
+		User save = userRepository.save(user);
+		System.out.println(save);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtAuthenticationServerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		createUser();
+	}
 }
--- JWT_Authentication_Server/src/main/java/com/jwt/services/CustomUserDetailsService.java
+++ JWT_Authentication_Server/src/main/java/com/jwt/services/CustomUserDetailsService.java
@@ -1,6 +1,9 @@
 package com.jwt.services;
 
-import org.springframework.security.core.userdetails.User;
+import com.jwt.model.CustomUserDetails;
+import com.jwt.model.User;
+import com.jwt.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,14 +13,30 @@ import java.util.ArrayList;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (username.equals("Pranav")){
-            return new User("Pranav","Pranav123", new ArrayList<>());
+
+        final User user = this.userRepository.findByUsername(username);
+
+        if (user==null){
+            throw new UsernameNotFoundException("User not found!!");
         }
-        else {
-            throw new UsernameNotFoundException("User not found !!");
+        else{
+            return new CustomUserDetails(user);
         }
+
+
+        //user database
+
+//        if (username.equals("Pranav")){
+//            return new User("Pranav","Pranav123", new ArrayList<>());
+//        }
+//        else {
+//            throw new UsernameNotFoundException("User not found !!");
+//        }
 
     }
 }
