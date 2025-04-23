9147fa6275501221f2e584283ae06822c8120cfc
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-23 15:07:31 +0530

    Spring Boot

---
+++ BootUnitTesting/src/main/java/com/unit/test/BootUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.unit.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootUnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootUnitTestingApplication.class, args);
+	}
+
+}
