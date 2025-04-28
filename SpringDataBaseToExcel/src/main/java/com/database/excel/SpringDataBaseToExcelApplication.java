ff54397fce7d2372898b1d885590564cbf7bd320
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-28 16:01:30 +0530

    Spring Boot

---
+++ SpringDataBaseToExcel/src/main/java/com/database/excel/SpringDataBaseToExcelApplication.java
@@ -0,0 +1,13 @@
+package com.database.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataBaseToExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataBaseToExcelApplication.class, args);
+	}
+
+}
---
+++ SpringDataBaseToExcel/src/test/java/com/database/excel/SpringDataBaseToExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataBaseToExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
