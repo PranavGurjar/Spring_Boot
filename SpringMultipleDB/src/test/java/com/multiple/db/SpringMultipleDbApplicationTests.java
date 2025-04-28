6e93d65093e28ae3df5bae9dc4eadf45ca1d368c
PranavRM5 <pranavmahajan510@gmail.com>
2025-04-28 13:14:32 +0530

    Spring Boot

---
+++ SpringMultipleDB/src/test/java/com/multiple/db/SpringMultipleDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiple.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultipleDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
